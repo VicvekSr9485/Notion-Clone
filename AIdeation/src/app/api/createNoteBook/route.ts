55a22605506547e7a754d976ad080dc08c78eb50
VicvekSr9485 <olamideoso65@gmail.com>
2023-10-22 06:23:05 +0100

    [feats]: add firebase config and complete editor

--- AIdeation/src/app/api/createNoteBook/route.ts
+++ AIdeation/src/app/api/createNoteBook/route.ts
@@ -1,5 +1,3 @@
-// /api/createNoteBook
-
 import { db } from "@/lib/db";
 import { $notes } from "@/lib/db/schema";
 import { generateImage, generateImagePrompt } from "@/lib/openai";
--- AIdeation/src/app/api/uploadToFirebase/route.ts
+++ AIdeation/src/app/api/uploadToFirebase/route.ts
@@ -1,5 +1,6 @@
 import { db } from "@/lib/db"
 import { $notes } from "@/lib/db/schema"
+import { uploadFileToFirebase } from "@/lib/firebase"
 import { eq } from "drizzle-orm"
 import { NextResponse } from "next/server"
 
@@ -13,7 +14,14 @@ export async function POST(req: Request) {
         if (!notes[0].imageUrl) {
             return new NextResponse('No image url', {status: 400})
         }
+        const firebase_url = await uploadFileToFirebase(notes[0].imageUrl, notes[0].name)
+        await db.update($notes).set({
+            imageUrl: firebase_url,
+        })
+        .where(eq($notes.id, parseInt(noteId)));
+        return new NextResponse("ok", { status: 200 });
     } catch (error) {
-        
+        console.log(error)
+        return new NextResponse("error", { status: 500});
     }
 }
