3ac07153a0b2a60bda7b7de87f906c239d822097
VicvekSr <Olamideoso65@gmail.com>
2023-10-23 04:02:43 +0000

    [feat]: correct dashboard routing to /notebook/:id intead of /notes/:id

--- AIdeation/src/app/dashboard/page.tsx
+++ AIdeation/src/app/dashboard/page.tsx
@@ -49,7 +49,7 @@ const DashboardPage = async (props: Props) => {
                     <CreateNoteDialog />
                     {notes.map(note => {
                         return (
-                            <a href={`/notes/${note.id}`} key={note.id}>
+                            <a href={`/notebook/${note.id}`} key={note.id}>
                                 <div className="border border-stone-300 rounded-lg overflow-hidden flex flex-col hover:shadow-xl transition hover:-translate-y-1">
                                     <Image 
                                     width={400}
